f14c04c1513e8b7966bbf9cbd5d8b63a5136c1b1
seanbenkenstein <seanbenkenstein@gmail.com>
2021-03-31 02:06:34 +1100

    drag and drop, remove feature v1

--- client/scripts/indexApp.js
+++ client/scripts/indexApp.js
@@ -1,7 +1,28 @@
 let hamburgerIcon = document.querySelector('.hamburger-icon')
 let sideBar = document.querySelector('.side-bar')
+let trashCan = document.querySelector('.trash-can')
+let mouseOverTrashCan = false;
 
 hamburgerIcon.addEventListener('click', () => {
     sideBar.classList.toggle('nav-show')
-    console.log(sideBar)
 })
+
+function showTrashCan(){
+    trashCan.classList.add('show-trash-can')
+}
+
+function hideTrashCan(){
+    trashCan.classList.remove('show-trash-can')
+}
+
+function setDragEnterTrashCan(){
+    console.log('mouse drag on trashcan')
+    trashCan.classList.add('drag-on-trash-can')
+    mouseOverTrashCan = true
+}
+
+function setDragLeaveTrashCan(){
+    console.log('mouse drag out trashcan')
+    trashCan.classList.remove('drag-on-trash-can')
+    mouseOverTrashCan = false
+}
