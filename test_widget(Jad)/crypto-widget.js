6c47d324ff0c8d0d7ab5776a702a30f4e8779d75
jt <jtabba@hotmail.com>
2021-03-30 17:49:26 +1100

    Updated crypto widget.

--- test_widget(Jad)/crypto-widget.js
+++ test_widget(Jad)/crypto-widget.js
@@ -1,102 +1,47 @@
 
 function displayCryptoPrice() {
 
-    let crypto = axios.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD').then(req, res => {
-        console.log(res.data)
+    axios.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD').then(res => {
+        let cryptoResults = res.data.DISPLAY.BTC.USD
+        let cryptoName = cryptoResults.FROMSYMBOL
+        let cryptoImage = cryptoResults.IMAGEURL
+        let cryptoPrice = cryptoResults.PRICE
+        let cryptoDayHigh = cryptoResults.HIGHDAY
+        let cryptoDayLow = cryptoResults.LOWDAY
+        let cryptoDayChange = cryptoResults.CHANGEPCT24HOUR
+        console.log(cryptoResults)
+        
+        cryptoWidgetHTML = `
+                    <div>
+                        <div class="crypto-id">
+                            <img src="https://www.cryptocompare.com/${cryptoImage}" class="crypto-image">
+                            <span class="name">${cryptoName}</span>
+                        </div>
+                        <div class="crypto-price">
+                            <span class="crypto-value-descriptor">Current price: </span>
+                            <span class="price">${cryptoPrice}</span>
+                        </div>
+                        <div class="crypto-day-high">
+                            <span class="crypto-value-descriptor">24 hour high: </span> 
+                            <span class="day-high">${cryptoDayHigh}</span>
+                        </div>
+                        <div class="crypto-day-low">
+                            <span class="crypto-value-descriptor">24 hour low: </span>
+                            <span class="day-low">${cryptoDayLow}</span>
+                        </div>
+                        <div class="crypto-day-change">
+                            <span class="crypto-value-descriptor">24 hour change: </span>
+                            <span class="day-change">${cryptoDayChange}%</span>
+                        </div>
+                    </div>
+                `
+
+        let cryptoWidget = document.createElement('div')
+        cryptoWidget.classList.add('crypto-widget')
+        cryptoWidget.innerHTML = cryptoWidgetHTML
+        document.body.append(cryptoWidget)
     })
 
-    cryptoWidgetHTML = `
-    <div>
-        <div class="crypto-id">
-            <p class="image"></p>
-            <img src="" alt="">
-        </div>
-        <div class="crypto-price">
-            <p class="price"></p>
-        </div>
-        <div class="crypto-day-high">
-            <p class="day-high"></p>
-        </div>
-        <div class="crypto-day-low">
-            <p class="day-low"></p>
-        </div>
-    </div>
-    `
 }
 
-
-
-function createTime() {
-    
-    let d = new Date()
-    let hours = d.getHours()
-    let minutes = d.getMinutes()
-    let seconds = d.getSeconds()
-    
-    minutes = minutes.toString()
-    if (minutes.length === 1) {
-        minutes = "0" + minutes
-    }
-    
-    seconds = seconds.toString()
-    if (seconds.length === 1) {
-        seconds = "0" + seconds
-    }
-    
-    clockWidgetHTML = `
-            <div class="time">
-                <div class="time-hours">
-                    <p class="p-hours">${hours}</p>
-                </div>
-                <div class="time-break">
-                    <p class="p-break">:</p>
-                </div>
-                <div class="time-mins">
-                    <p class="p-mins">${minutes}</p>
-                </div>
-                <div class="time-break">
-                    <p class="p-break">:</p>
-                </div>
-                <div class="time-secs">
-                    <p class="p-secs">${seconds}</p>
-                </div>
-            </div>
-        `
-
-    let clockWidget = document.createElement('div')
-    clockWidget.classList.add('clock-widget')
-    clockWidget.innerHTML = clockWidgetHTML
-    document.body.append(clockWidget)
-
-}
-
-createTime()
-
-const getTime = function(){
-
-    const pHours = document.querySelector('.p-hours')
-    const pMins = document.querySelector('.p-mins')
-    const pSecs = document.querySelector('.p-secs')
-
-    let d = new Date()
-    let hours = d.getHours()
-    let minutes = d.getMinutes()
-    let seconds = d.getSeconds()
-
-    minutes = minutes.toString()
-    if (minutes.length === 1) {
-        minutes = "0" + minutes
-    }
-
-    seconds = seconds.toString()
-    if (seconds.length === 1) {
-        seconds = "0" + seconds
-    }
-
-    pHours.textContent = hours
-    pMins.textContent = minutes
-    pSecs.textContent = seconds
-    setTimeout(getTime, 1000);
-};
-
-setTimeout(getTime, 1000);
+displayCryptoPrice()
