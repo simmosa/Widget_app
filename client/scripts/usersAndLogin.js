76e699c3ff0e45457161d8ffaf5f1134772f394b
Simon <simonraj@bigpond.net.au>
2021-03-31 12:07:44 +1100

    login, logout, sign up working

---
+++ client/scripts/usersAndLogin.js
@@ -0,0 +1,100 @@
+//////////////                                     //////////////////////
+/////////////       login from and handlers       //////////////////////
+
+const loginForm = document.querySelector('.login_form')
+const loginEmailInput = document.querySelector('.login_email_input')
+const loginPasswordInput = document.querySelector('.login_password_input')
+
+function handleLogin(e) {
+    e.preventDefault()
+
+    axios.post('/api/sessions', { email: loginEmailInput.value, password: loginPasswordInput.value}).then(res => {
+        if (res.data.login == 'success') {  
+            displayWelcomeUser(res.data.name)
+        }
+    })
+}
+
+loginForm.addEventListener('submit', handleLogin)
+
+
+///////////                                            ///////////////////
+//////////          new user from and handler         ///////////////////
+
+const signupForm = document.querySelector('.signup_form')
+const firstNameInput = document.querySelector('.first_name_input')
+const lastNameInput = document.querySelector('.last_name_input')
+const emailInput = document.querySelector('.email_input')
+const passwordInput = document.querySelector('.password_input')
+
+function handleNewUser(e) {
+    e.preventDefault()
+
+    axios.post('/api/users', {
+        firstName: firstNameInput.value, 
+        lastName: lastNameInput.value, 
+        email: emailInput.value, 
+        password: passwordInput.value
+    }).then(res => {
+        displayWelcomeUser(res.data.name)         
+    });  
+}
+
+signupForm.addEventListener('submit', handleNewUser)
+
+
+///////////////                           ///////////////
+//////////////      logout handler       ///////////////
+
+const logoutBtn = document.querySelector('.logout-btn')
+
+function handleLogout() {
+    // save the widgets that the user has enabled
+    // saveUserState()
+    displayLogoutState()
+}
+
+logoutBtn.addEventListener('click', handleLogout)
+
+
+////////////////
+///////////////      display functions
+
+function displayWelcomeUser(name) { 
+    //displays user name and hides login and signup
+    const welcomeUserDiv = document.querySelector('.welcome-user-div')
+    welcomeUserDiv.classList.remove('hide-logins-items')           
+    let welcomeMessageH4 = document.createElement('h4')
+    welcomeMessageH4.textContent = `Welcome, ${name}`
+    welcomeUserDiv.prepend(welcomeMessageH4)
+
+    const loginDiv = document.querySelector('.login-div')
+    loginDiv.classList.add('hide-logins-items')
+    const signupDiv = document.querySelector('.signup-div')
+    signupDiv.classList.add('hide-logins-items')
+}
+
+function displayLogoutState() {
+    //reset the display to no user logged in
+    const welcomeUserDiv = document.querySelector('.welcome-user-div')
+    welcomeUserDiv.classList.add('hide-logins-items')    
+    welcomeUserDiv.removeChild(welcomeUserDiv.childNodes[0]);      
+
+    const loginDiv = document.querySelector('.login-div')
+    loginDiv.classList.remove('hide-logins-items')
+    const signupDiv = document.querySelector('.signup-div')
+    signupDiv.classList.remove('hide-logins-items')
+
+    // clear the forms
+    loginEmailInput.value = ""
+    loginPasswordInput.value = ""
+    firstNameInput.value = ""
+    lastNameInput.value = ""
+    emailInput.value = ""
+    passwordInput.value = ""
+}
+
+
+
+
+
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,29 +4,38 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 
+
+function userLoggedIn(req, res) {}
+
 function newSession(req, res) {
     pool.query('SELECT * FROM users Where email = $1;', [req.body.email], (err, db) => {
         if (db.rows.length<1) { 
-            res.json("No user with this email in the system")
+            res.json( { login: 'no email match' })
         } else {
             (bcrypt.compare(req.body.password, db.rows[0].password, function(err, result) {
                 if (result == true) {
                     req.session.user_id = db.rows[0].id
-                    res.json(`logged in.. User id is ${db.rows[0].id}. Session id is ${req.session.user_id}`)
+                    res.json({ login: 'success', name: `${db.rows[0].first_name}`})
                 } else if (result == false) {
-                    res.json("incorrect credentials provided.")
+                    res.json({ login: 'password incorrect' })
                 }
-        }))   } 
+            }))
+        } 
     })
 }
 
+function endSession(req, res) {
+    // save the loaded widgets
+    //destroy the session
+    // req.session.destroy()
+}
 
-function create(req,res) {
+function createUser(req,res) {
     bcrypt.genSalt(saltRounds, function(err, salt) { 
         bcrypt.hash(req.body.password, salt, function(err, passwordDigest) {
             pool.query(
                 'INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) returning *', [req.body.firstName, req.body.lastName, req.body.email, passwordDigest], (err, db) => {
-                    res.json( {user: db.rows[0]} )
+                    res.json( {user: db.rows[0], name: db.rows[0].first_name} )
             })
         }); 
     }); 
@@ -34,6 +43,8 @@ function create(req,res) {
 
 
 module.exports = {
+    userLoggedIn,
     newSession,
-    create
+    endSession,
+    createUser
 }
